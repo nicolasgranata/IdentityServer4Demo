2940de637a14ca7cd93eaa52734beaf68d3bcd17
nicolasgranata <nicolasegranata@gmail.com>
2019-06-30 21:21:57 -0300

    Initial Commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,77 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to start!");
+
+            Console.ReadKey();
+
+            Console.WriteLine("\nProcessing...\n");
+
+            var accessToken = Task.Run( () => GetAccessTokenAsync()).Result;
+
+            Console.WriteLine($"Access Token: {accessToken}");
+
+            var response = Task.Run(() => GetValues(accessToken)).Result;
+
+            Console.WriteLine($"\n{response}");
+        }
+
+        private static async Task<string> GetAccessTokenAsync()
+        {
+            var client = new HttpClient();
+
+            // discover endpoints from metadata
+            var discover = await client.GetDiscoveryDocumentAsync("http://localhost:5000");
+            if (discover.IsError)
+            {
+                return discover.Error;
+            }
+
+            // request token
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = discover.TokenEndpoint,
+                ClientId = "consoleClient",
+                ClientSecret = "secret",
+                Scope = "api1"
+            });
+
+            if (tokenResponse.IsError)
+            {
+                return tokenResponse.Error;
+            }
+
+            return tokenResponse.AccessToken;
+        }
+
+        private static async Task<string> GetValues(string accessToken)
+        {
+            // call api. TODO change the HttpClient by the HttpClienFactory
+            var client = new HttpClient();
+
+            client.SetBearerToken(accessToken);
+
+            var response = await client.GetAsync("http://localhost:52618/api/values");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return response.StatusCode.ToString();
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+
+                return JArray.Parse(content).ToString();
+            }
+        }
+    }
+}
---
+++ IdentityServer/Config.cs
@@ -0,0 +1,92 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.Models;
+using System.Collections.Generic;
+using IdentityServer4.Test;
+
+namespace IdentityServer4
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApis()
+        {
+            return new ApiResource[]
+            {
+                new ApiResource("api1", "API 1")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new Client[]
+            {
+                new Client
+                {
+                    ClientId = "consoleClient",
+
+                    // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    // secret for authentication
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    // scopes that client has access to
+                    AllowedScopes = { "api1" }
+                },
+
+                 // OpenID Connect implicit flow client (MVC)
+                new Client
+                {
+                    ClientId = "mvcWebApp",
+                    ClientName = "MVC Client",
+                    AllowedGrantTypes = GrantTypes.Implicit,
+
+                    // where to redirect to after login
+                    RedirectUris = { "http://localhost:58791/signin-oidc" },
+
+                    // where to redirect to after logout
+                    PostLogoutRedirectUris = { "http://localhost:58791/signout-callback-oidc" },
+
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                    }
+                }
+           };
+        }
+
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "alice",
+                    Password = "password"
+                },
+                new TestUser
+                {
+                    SubjectId = "2",
+                    Username = "bob",
+                    Password = "password"
+                }
+            };
+         }
+    }
+}
